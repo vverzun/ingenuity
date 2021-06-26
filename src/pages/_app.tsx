cc6724e8f63db91bc3294017080eeebea416c26f
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-06-26 23:28:40 +0300

    feat: add custom theme and font for the chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@styles';
+import '@fontsource/bebas-neue';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
