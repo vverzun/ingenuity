bfc469235438badabbb832147a23a75172f8ed6a
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-06-26 17:49:31 +0300

    build: add chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
