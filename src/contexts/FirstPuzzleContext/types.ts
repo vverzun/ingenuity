9d1001250c51b2479bd593b5828fc3a5c415e4ec
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-06-30 11:45:40 +0300

    feat: add FirstPuzzleContext

---
+++ src/contexts/FirstPuzzleContext/types.ts
@@ -0,0 +1,4 @@
+export type FirstPuzzleContextValue = {
+  areLightsOn: boolean;
+  enteredLetters: string[];
+};
---
+++ src/contexts/types.ts
@@ -0,0 +1,1 @@
+export type { FirstPuzzleContextValue } from './FirstPuzzleContext';
