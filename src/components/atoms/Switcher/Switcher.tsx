b2d57a148e4106209bff3ec85ee37993fbf38ec1
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-07-02 20:39:55 +0300

    feat: add switcher assets with a better quality

--- src/components/atoms/Switcher/Switcher.tsx
+++ src/components/atoms/Switcher/Switcher.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Image } from '@chakra-ui/react';
+import { Img } from '@chakra-ui/react';
 import type { SwitcherSound, SwitcherProps } from './types';
 
 const Switcher: FC<SwitcherProps> = ({ isOn, onSwitcherClick }) => {
@@ -25,10 +25,11 @@ const Switcher: FC<SwitcherProps> = ({ isOn, onSwitcherClick }) => {
   };
 
   return (
-    <Image
-      src={isOn ? '/pngs/on.png' : '/pngs/off.png'}
+    <Img
+      width="100%"
+      height="auto"
+      src={isOn ? '/svgs/on.svg' : '/svgs/off.svg'}
       alt="light switch"
-      ignoreFallback
       onClick={onClick}
     />
   );
