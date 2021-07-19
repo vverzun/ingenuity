fddc5598ea3203d4456f69fd8dcc3ae2565e14b2
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-07-20 01:29:51 +0300

    feat: add secret letters for the second puzzle

--- src/constants/secret/secret.ts
+++ src/constants/secret/secret.ts
@@ -1,6 +1,7 @@
 import { Letter } from '@atoms/types';
 
 export const SECRET_WORD_LETTERS: Letter[] = [
+  // UNDERLAY_LETTERS
   { id: '89641f4e-4560-4798-b390-301d6f76006a', symbol: 'S' },
   { id: 'f25cfab8-31f3-46a0-abb0-f6bf96584779', symbol: 'E' },
   { id: '8adc2866-c11b-4e1d-8e24-4318ce059f7d', symbol: 'Q' },
@@ -9,4 +10,31 @@ export const SECRET_WORD_LETTERS: Letter[] = [
   { id: '566fa384-ad4b-41d6-9702-bc7e2be49c7c', symbol: 'N' },
   { id: '49483e98-4199-4365-b1b0-1e570acef2de', symbol: 'C' },
   { id: 'f1211075-0b7c-4e82-847f-4baf1466bd1b', symbol: 'E' },
+];
+
+export const MYSTIC_SQUARE_LETTERS: Array<Letter[]> = [
+  [
+    { id: '7e0428db-0781-47aa-8253-8541f9f2d63d', symbol: 'R' },
+    { id: '85f5bf9d-f0ea-435b-bab8-5875a0b6965b', symbol: 'A' },
+    { id: '8761e15a-db92-478a-af7a-0b681caae862', symbol: 'T' },
+    { id: '7f6db1a8-d981-4ccf-83fc-bf0bbaafc7cb', symbol: 'E' },
+  ],
+  [
+    { id: '6deecb4b-5070-4e5e-aa02-eb8985022a73', symbol: 'Y' },
+    { id: '34546ae8-4930-4c52-8be6-643b405ee69f', symbol: 'O' },
+    { id: '7072395a-f049-4b0c-a58e-3ab2d3281812', symbol: 'U' },
+    { id: '7c413811-5648-410d-b8dd-9fbd8c395c3c', symbol: 'R' },
+  ],
+  [
+    { id: 'fed79fd6-531e-4346-88e3-88372eb7c7a2', symbol: 'M' },
+    { id: 'f3dfbe6e-3c64-4a66-8000-7ddb4ef8d7eb', symbol: 'I' },
+    { id: '7c191265-6da1-4317-bc85-1fb5ddab28ad', symbol: 'N' },
+    { id: 'b455218d-0f2c-4e4f-a31c-df2c1292d1cb', symbol: 'D' },
+  ],
+  [
+    { id: '76cbe19d-84bf-4689-87e7-03e6b6c0a7ee', symbol: 'P' },
+    { id: 'ed42cbae-e1e2-464f-a3fb-5f8fb6e7a930', symbol: 'A' },
+    { id: 'd2a1ef54-dd05-4bb3-b9df-8a995f9e6194', symbol: 'L' },
+    { id: '5227c47f-fa65-4ddc-aea6-94b21e4137ee', symbol: 'empty' },
+  ],
 ];
