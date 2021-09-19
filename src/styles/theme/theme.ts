a5f0c11838a85da513161d415aecbcb59ebf2cc8
Vladyslav Verzun <vladyslav.verzun@gmail.com>
2021-09-19 18:26:27 +0300

    feat: use chakra color mode instead of context

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,9 +1,20 @@
 import type { ThemeConfig } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+import type { StyleFunctionProps } from '@chakra-ui/theme-tools';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
+};
+
+const styles = {
+  global: (props: StyleFunctionProps) => ({
+    body: {
+      backgroundColor: mode('white', 'black')(props),
+      transition: 'background-color 0.2s ease',
+    },
+  }),
 };
 
 const theme = extendTheme({
@@ -12,6 +23,7 @@ const theme = extendTheme({
     heading: 'Major Mono Display',
     body: 'Major Mono Display',
   },
+  styles,
 });
 
 export default theme;
